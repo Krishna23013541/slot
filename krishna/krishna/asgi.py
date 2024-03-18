8326534e49b9eadf36587c73b854fe0bd0d1188e
krishna23013541 <fierystud16@gmail.com>
2024-03-18 14:56:22 +0530

    Success

--- krishna/krishna/asgi.py
+++ krishna/krishna/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for krish project.
+ASGI config for krishna project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krishna.settings')
 
 application = get_asgi_application()
--- krishna/krishna/wsgi.py
+++ krishna/krishna/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for krish project.
+WSGI config for krishna project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krishna.settings')
 
 application = get_wsgi_application()
